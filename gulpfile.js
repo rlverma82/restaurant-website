daa579b3ebb5297fd2e7627c7c5cdb984b9ebe35
Rahul Verma <rl_verma82@yahoo.com>
2020-07-28 01:45:57 +0530

    fixed title of contact us page

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,74 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin');
 
-   gulp.task('sass', function () {
-     return gulp.src('./css/*.scss')
-       .pipe(sass().on('error', sass.logError))
-       .pipe(gulp.dest('./css'));
-   });
 
-   gulp.task('sass:watch', function () {
-     gulp.watch('./css/*.scss', ['sass']);
-   });
+  gulp.task('sass', function () {
+    return gulp.src('./css/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+  });
 
-   gulp.task('browser-sync', function () {
-      var files = [
-         './*.html',
-         './css/*.css',
-         './img/*.{png,jpg,gif}',
-         './js/*.js'
-      ];
+  gulp.task('sass:watch', function(){
+      gulp.watch('./css/*.scss', gulp.series('sass'));
+  });
 
-      browserSync.init(files, {
-         server: {
-            baseDir: "./"
-         }
-      });
+  gulp.task('browser-sync', function () {
+     var files = [
+        './*.html',
+        './css/*.css',
+        './img/*.{png,jpg,gif}',
+        './js/*.js'
+     ];
 
-   });
+     browserSync.init(files, {
+        server: {
+           baseDir: "./"
+        }
+     });
+
+  });
 
    // Default task
-   gulp.task('default', ['browser-sync'],'sass:watch');
+   gulp.task('default', gulp.parallel('browser-sync', function() {
+       gulp.series('sass:watch')();
+   }));
+
+   // Clean
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('copyfonts', function() {
+   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/fonts'));
+});
+
+// Images
+gulp.task('imagemin', function() {
+  return gulp.src('img/*.{png,jpg,gif}')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+  return gulp.src('./*.html')
+  .pipe(flatmap(function(stream, file){
+      return stream
+        .pipe(usemin({
+            css: [ rev() ],
+            html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+            js: [ uglify(), rev() ],
+            inlinejs: [ uglify() ],
+            inlinecss: [ cleanCss(), 'concat' ]
+        }))
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build',['clean'], function() {
+    gulp.start('copyfonts','imagemin','usemin');
+});
